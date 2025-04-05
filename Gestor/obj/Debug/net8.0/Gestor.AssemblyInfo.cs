6b89539c210b193883e963d701e4b672b43a3423
Allendes Kevin <kevinallendes1993@gmail.com>
2025-04-05 15:40:38 -0300

    Commit
    commit

--- Gestor/obj/Debug/net8.0/Gestor.AssemblyInfo.cs
+++ Gestor/obj/Debug/net8.0/Gestor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ee21f8e9927f14403b05626b72f2e5adcf14f78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52bccad42b0b6d55072ddf908e9aa3a2969fe2a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
